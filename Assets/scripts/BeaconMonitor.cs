fd6014b3a83df5da44691212a4ae0ad1ff561f67
Juliano Pacheco <julianopache@gmail.com>
2016-08-09 10:53:17 -0300

    Commit first version

---
+++ Assets/scripts/BeaconMonitor.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+
+public class BeaconMonitor : MonoBehaviour 
+{
+    public Text beaconDistanceText;
+    public Text batteryIcon; 
+
+    static readonly string BATTERY_ICON  = Char.ConvertFromUtf32(0xf243);
+
+ 	void Start ()
+	{
+		print ("Start");
+	}
+
+	// Update is called once per frame
+	void Update () 
+    {
+		print ("Update");
+	   	UpdateStatusIndicators();
+	}
+    
+    /// <summary>
+    /// Find the current beacon device battery level and update indicators in the 
+    /// UI accordingly.
+    /// </summary>
+    void UpdateStatusIndicators()
+    {
+    	// set battery icon
+		batteryIcon.text = BATTERY_ICON;
+
+		// set beacon informations
+		string uuid = "8492E75F-4FD6-469D-B132-04FE94921D8";
+		string major = "12768";
+		string minor = "3289";
+
+		// invoke FarolBeaconPlugin
+		var distance = FarolBeaconPlugin.GetDistance(uuid, major, minor);
+
+//		var distance = FarolBeaconPlugin.GetText();
+
+
+		// set beacon distance
+		beaconDistanceText.text = distance;
+    }
+}
---
+++ Assets/scripts/FarolBeaconPlugin.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class FarolBeaconPlugin {
+
+	public FarolBeaconPlugin ()
+	{
+		
+	}
+
+	/// <summary>
+    /// Test parameter pass to android.
+    /// </summary>
+	public static string GetText()
+	{
+		if (Application.platform == RuntimePlatform.Android)
+        {
+            using (var javaUnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+            {
+                using (var currentActivity = javaUnityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+                {
+					using (var androidPlugin = new AndroidJavaObject("com.menvia.farolbeacon.plugin.AndroidPlugin", currentActivity))
+                    {
+						return androidPlugin.Call<string>("getText", new object[]{"Passando parametro string"});
+                    }
+                }
+            }
+        }
+	    return "Sem Retorno";
+	}
+
+	/// <summary>
+    /// Get beacon distance information.
+    /// </summary>
+	public static string GetDistance(string uuid, string major, string minor)
+	{
+		if (Application.platform == RuntimePlatform.Android)
+        {
+            using (var javaUnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+            {
+                using (var currentActivity = javaUnityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+                {
+					using (var androidPlugin = new AndroidJavaObject("com.menvia.farolbeacon.plugin.AndroidPlugin", currentActivity))
+                    {
+						return androidPlugin.Call<string>("getDistance", new object[]{uuid, major, minor});
+                    }
+                }
+            }
+        }
+	    return "Beacon não encontrado";
+	}
+}
