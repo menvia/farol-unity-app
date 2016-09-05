62222feb97405d98411d80767d0b35758a31d96d
adriano.kerber <adriano.kerber@menvia.com>
2016-09-05 13:33:35 -0300

    Fix plugin FarolWS and example script

--- Assets/scripts/WS/Example_FarolControllerUse.cs
+++ Assets/scripts/WS/Example_FarolControllerUse.cs
@@ -1,35 +1,65 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
 using Farol;
 using Farol.Models;
-using System;
 
 public class Example_FarolControllerUse : MonoBehaviour {
+    string text;
 
     User user;
 
+    public void onCompleteDeviceInfo(DeviceInfo[] s)
+    {
+        if (s.Length > 0)
+        {
+            Debug.Log("DEVICE INFO: " + s[0]._id);
+            //text = s[0]._id;
+
+            string str = "Device info: ";
+            foreach (DeviceInfo d in s)
+            {
+                str = " { _id = " + d._id + ", ";
+                str += "triggers[0]._id = " + d.triggers[0]._id + ", ";
+                str += "macaddress = " + d.macaddress;
+
+                str += " }, ";
+
+                text += str;
+            }
+        }
+    }
     public void onCompleteDeviceL(Device[] s)
     {
         if (s.Length > 0)
-            Debug.Log("DEVICES: "+s[0]._id);
+        {
+            Debug.Log("DEVICES: " + s[0]._id);
+            text = s[0]._id;
+        }
     }
     public void onCompleteLocationL(Location[] s)
     {
         if(s.Length > 0)
-            Debug.Log("LOCATION: " + s[0].city);
+            Debug.Log("LOCATIONS: " + s[0].city);
     }
     public void onCompleteTriggerL(Trigger[] s)
     {
         if (s.Length > 0)
-            Debug.Log("TRIGGER TYPE: " + s[0].type);
+        {
+            Debug.Log("TRIGGER TYPES: " + s[0].type);
 
-        FarolController.CheckIn(s[0], user, onSuccessCheckIn, onFailureCheckIn);
-        FarolController.CheckOut(s[0], user, onSuccessCheckOut, onFailureCheckIn);
+            if (user != null)
+            {
+                FarolController.CheckIn(s[0], user, onSuccessCheckIn, onFailureCheckIn);
+                FarolController.CheckOut(s[0], user, onSuccessCheckOut, onFailureCheckIn);
+            }
+        }
+        Debug.Log("Não retornou um trigger...");
     }
 
     public void onCompleteApp(User user)
     {
         Debug.Log("USER");
+        GameObject o = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        o.transform.position = new Vector3();
         if (user != null)
         {
             Debug.Log(" created! ");
@@ -62,17 +92,29 @@ public class Example_FarolControllerUse : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+        /*
+         * NOTE: To use the FarolWS you will need to access through FarolController.
+         * STEPS:
+         * 1) Instantiate the class with your app token;
+         * 2) Access the utility methods passing your callback's signature (That will be called when the service responds).
+         * 3) Follow the examples below.
+         */
 
-        // Inicialização da singleton
+        /// Inicialização da singleton
         FarolController.Instantiate("YOUR_APP_TOKEN_HERE");
 
-        // Pegando lista de dispositivos, localizações e gatilhos
+        /// Pegando lista de dispositivos, localizações e gatilhos
         //FarolController.DeviceList(onCompleteDeviceL);
         //FarolController.LocationList(onCompleteLocationL);
         //FarolController.TriggerList(onCompleteTriggerL);
+        FarolController.DeviceInfoList(onCompleteDeviceInfo);
 
-        // Cria um novo usuário | A callback onComplete recebe o usuário criado (OBS: só recebe se ele não existir)
-        FarolController.AppUser("Nome", "Sobrenome", "test@test.com", onCompleteApp);
-        
+        /// Cria um novo usuário | A callback onComplete recebe o usuário criado (OBS: só recebe se ele não existir)
+        //FarolController.AppUser("Nome", "Sobrenome", "test@test.com", onCompleteApp);
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(0,0,Screen.width,Screen.height), ("Device id of device list: "+text));
     }
 }
